1a26525f78a54025a443561c59e31e4a5df5cfc8
r0b0iv0 <ivo10water@gmail.com>
2023-06-30 13:05:05 +0300

    added charge dash mechanic + the animations for the asteroids

--- Assets/DeathEvent.cs
+++ Assets/DeathEvent.cs
@@ -19,7 +19,7 @@ public class DeathEvent : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {   
-        animator.Play("UFO_attack");
+        // animator.Play("UFO_attack");
         Destroy(animator.gameObject);
     }
 
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     [SerializeField] private Animator animator;
+    [SerializeField] private Animator barAnimator;
     private Vector2 startPos;
     private Vector2 endPos;
     private Touch touch;
@@ -14,43 +15,45 @@ public class PlayerMovement : MonoBehaviour
     private bool fingerDown;
     public float velocity = 7.5f;
     private ParticleSystem particleSystem;
-    [SerializeField]private float dashTime = 0.25f;
-    [SerializeField]private float dashPower = 10f;
-    [SerializeField]private float dashCooldown = 2f;
+    [SerializeField] private float dashTime = 0.25f;
+    [SerializeField] private float dashPower = 10f;
+    [SerializeField] private float dashCooldown = 2f;
     private bool canDash = true;
-    
+
     public bool isDashing = false;
 
 
     void Start()
-    {   
+    {
         particleSystem = GetComponentInChildren<ParticleSystem>();
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = Vector2.right * velocity;
     }
     void Update()
     {
-       if (Input.GetKeyDown(KeyCode.RightArrow) && canDash)
+        if (Input.GetKeyDown(KeyCode.RightArrow) && canDash)
         {
             StartCoroutine(Dash());
 
         }
 
-       
+
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
 
-         if (isDashing == false) {
+        if (isDashing == false)
+        {
             rb.velocity = new Vector2(velocity + 0.00001f, rb.velocity.y);
         }
-         
+
         if (Input.GetKey(KeyCode.UpArrow))
         {
             rb.velocity = new Vector2(velocity + 0.00001f, (velocity * 0.6f));
 
         }
-       
+
     }
 
     public void ressetMovement()
@@ -59,20 +62,22 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = Vector2.right * velocity;
     }
 
-    private IEnumerator Dash() 
-    {   
-            canDash = false;
-            isDashing = true;
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
         animator.SetBool("Attack", true);
         rb.velocity = new Vector2(rb.velocity.x + dashPower, rb.velocity.y);
         particleSystem.Emit(10);
         rb.gravityScale = 0f;
         yield return new WaitForSeconds(dashTime);
         animator.SetBool("Attack", false);
+        barAnimator.SetBool("hasDashed", true);
         isDashing = false;
         rb.gravityScale = 0.8f;
 
         yield return new WaitForSeconds(dashCooldown);
+        barAnimator.SetBool("hasDashed", false);
         canDash = true;
     }
 
